000f42d2aa9c471ad2980fbf5f793bdbbbaab0a7
Jhozua Manguera Diaz <diaz.jhozua@gmail.com>
2022-03-08 23:30:20 +0800

    Added Inquiries

--- app/src/main/java/com/example/eserbisyo/Constants/Api.java
+++ app/src/main/java/com/example/eserbisyo/Constants/Api.java
@@ -1,8 +1,8 @@
 package com.example.eserbisyo.Constants;
 
 public class Api {
-//    public static final String URL = "https://barangay-cupang.herokuapp.com/";
-    public static final String URL = "http://192.168.1.3:8001/";
+    public static final String URL = "https://barangay-cupang.herokuapp.com/";
+//    public static final String URL = "http://192.168.1.3:8001/";
     public static final String HOME = URL+"api";
     public static final String LOGIN = HOME+"/login";
     public static final String REGISTER = HOME+"/register";
